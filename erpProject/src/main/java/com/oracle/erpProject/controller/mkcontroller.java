ea2b442cc69b4b345e368d4d7917ac233d84c289
minjjing0 <banggujoong@gmail.com>
2024-03-27 12:34:48 +0900

    controller service 주입 추가

--- erpProject/src/main/java/com/oracle/erpProject/controller/mkcontroller.java
+++ erpProject/src/main/java/com/oracle/erpProject/controller/mkcontroller.java
@@ -3,12 +3,15 @@ package com.oracle.erpProject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.oracle.erpProject.service.mk.MK_Service_interface;
+
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class mkcontroller {
 
+	private MK_Service_interface mk_Service_interface;
 	
 	@GetMapping("/mk")
 	public String mkPage() {
