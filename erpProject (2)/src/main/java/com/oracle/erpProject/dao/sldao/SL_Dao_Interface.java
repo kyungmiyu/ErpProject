6955a00d894f25003fc0312f3602622d1682c5dc
kyungmiyu <yu.km0304@gmail.com>
2024-03-29 17:59:08 +0900

    test commit

--- erpProject (2)/src/main/java/com/oracle/erpProject/dao/sldao/SL_Dao_Interface.java
+++
@@ -1,5 +0,0 @@
-package com.oracle.erpProject.dao.sldao;
-
-public interface SL_Dao_Interface {
-
-}
--- erpProject (2)/src/main/java/com/oracle/erpProject/service/slservice/SL_Service_Interface.java
+++
@@ -1,5 +0,0 @@
-package com.oracle.erpProject.service.slservice;
-
-public interface SL_Service_Interface {
-
-}
